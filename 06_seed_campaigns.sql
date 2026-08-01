3cb955612ad86e8f0690b017c100282cce656c4d
vedhass <vedhaskanade1978@gmail.com>
2026-08-01 17:39:33 +0530

    Create 06_seed_campaigns.sql

---
+++ 06_seed_campaigns.sql
@@ -0,0 +1,5 @@
+INSERT INTO campaigns VALUES
+(101, 10, 'Festival Mega Sale', 'Search', 150000),
+(102, 10, 'Pixel Launch Video', 'Video', 300000),
+(103, 20, 'Yoga Mat Display Run', 'Display', 50000),
+(104, 30, 'Coding Bootcamp Ads', 'Search', 200000);
